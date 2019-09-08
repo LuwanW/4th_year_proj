467a0e850269c2710012caefee68c7424498dae9
LuwanW <luwanwang@cmail.carleton.ca>
2019-09-08 15:54:40 -0400

    Update client_send_data_simulatiom.py

--- bin/client_send_data_simulatiom.py
+++ bin/client_send_data_simulatiom.py
@@ -15,6 +15,7 @@ s.connect((HOST, PORT))
 
 class Scope(object):
     def __init__(self, ax, maxt=2, dt=0.02):
+        #self object, ax graph, maxt max time frame, dt 0.02 capture frequency, this import data into function
         self.ax = ax
         self.dt = dt
         self.maxt = maxt
@@ -22,17 +23,24 @@ class Scope(object):
         self.ydata = [0]
         self.line = Line2D(self.tdata, self.ydata)
         self.ax.add_line(self.line)
+        # set amplitude limits
         self.ax.set_ylim(-.1, 1.1)
+        # set x axis limits 
         self.ax.set_xlim(0, self.maxt)
-
+        
+    # updates data
     def update(self, y):
+        #starts from last data (-1)
         lastt = self.tdata[-1]
         if lastt > self.tdata[0] + self.maxt:  # reset the arrays
+            # self.tdata[-1] is last value, the resultant array is size 1 with value lastT
             self.tdata = [self.tdata[-1]]
             self.ydata = [self.ydata[-1]]
+            #set frame to current point + maxt
             self.ax.set_xlim(self.tdata[0], self.tdata[0] + self.maxt)
             self.ax.figure.canvas.draw()
 
+        #get t for next point
         t = self.tdata[-1] + self.dt
         self.tdata.append(t)
         self.ydata.append(y)
@@ -41,12 +49,13 @@ class Scope(object):
         # dump data and send data through socket
         # todo: use real measured data instead of simulation
         print(t,y)
+        
         msg = [t,y]
         data=pickle.dumps(msg)
         s.send(data)
         return self.line,
 
-
+# to use remove random generator
 def emitter(p=0.03):
     'return a random value with probability p, else 0'
     while True:
